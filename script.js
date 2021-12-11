d2b5181f5d8abb0364684f92e617fa4392e979d5
Jordan Rothwell <jordanlrothwell@gmail.com>
2021-12-11 12:14:36 +1000

    Added arrays for upperCase; lowerCase; numbers; and special. Added choosePasswordLength function with prompt (requires improved validation).

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declares + initializes array of upper case characters
+const upperCase = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+
+// Declares + initializes array of lower case characters
+const lowerCase = upperCase.map(i => i.toLowerCase());
+
+// Declares + initializes array of digits 0 through 9
+const numbers = [...Array(10)].map((val, i) => String.fromCharCode(i + 48));
+
+// Declares + initializes array of special characters
+const special = [...Array(15)].map((val, i) => String.fromCharCode(i + 33)).concat([...Array(7)].map((val, i) => String.fromCharCode(i + 58)));
+
+// Prompts the user to choose a password length between 8 and 128 characters long
+function choosePasswordLength() {
+    var passwordLength = prompt("Choose your password length.");
+    if (passwordLength < 8) {
+      alert("Your password cannot be less than 8 characters.");
+      choosePasswordLength()
+    } else if (passwordLength > 128) {
+      alert("Your password cannot be more than 128 characters.")
+      choosePasswordLength()
+    } else {
+      console.log(passwordLength)
+        }
+    }
+
+choosePasswordLength()16
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
