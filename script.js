8b64e606eeb6c5f0b986242fbfc0b4750cb2a60b
Jordan Rothwell <jordanlrothwell@gmail.com>
2021-12-16 17:47:46 +1000

    Commented the script; removed the TODO list.

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// Assignment Code
+// Select our generate password button & assign it to a variable.
 var generateBtn = document.querySelector("#generate");
 
-// Declares + initializes array of upper case characters
+// Declare & initialise arrays of:
+// Upper case letters;
 const upperCase = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
 
-// Declares + initializes array of lower case characters
+// Lower case letters;
 const lowerCase = upperCase.map((i) => i.toLowerCase());
 
-// Declares + initializes array of digits 0 through 9
+// Digits 0 through 9; and
 const numbers = [...Array(10)].map((val, i) => String.fromCharCode(i + 48));
 
-// Declares + initializes array of special characters
+// Special characters.
 const special = [...Array(15)]
   .map((val, i) => String.fromCharCode(i + 33))
   .concat([...Array(7)].map((val, i) => String.fromCharCode(i + 58)));
 
-// Declares global placeholder variables for the functions which follow
+// Declare global placeholder variables for the functions which follow.
 var chosenLength;
 var chosenCategories;
 var finalPassword;
 
-// Prompts the user to choose a password length between 8 and 128 characters long
+// Prompt dialoge - ask the user to choose a password length between 8 and 128 characters long.
 const chooseLength = function (userInputLength) {
   var userInputLength = prompt("Choose your password length.");
-  if (userInputLength === null) {
-    return;
-  } else if (isNaN(userInputLength)) {
+  // Disallow non-numeric entries.
+  if (isNaN(userInputLength)) {
     alert("Please enter a number between 8 and 128.");
     chooseLength();
   } else if (userInputLength < 8) {
@@ -34,15 +34,17 @@ const chooseLength = function (userInputLength) {
   } else if (userInputLength > 128) {
     alert("Your password cannot be more than 128 characters.");
     chooseLength();
+    // Once we have a number meeting the criteria, assign it to the global variable we declared earlier.
   } else {
-    console.log(userInputLength);
     chosenLength = userInputLength;
   }
 };
 
-// Function to concatenate array(s) of upperCase; lowerCase; numbers; and special
+// Confirmation dialogue - ask the user to confirm which types of characters they would like in their password.
 const chooseCategories = function () {
+  // Create an empty array...
   var growingArray = [];
+  // ... then concatenate the arrays we declared above depending on the user's response.
   if (confirm("Do you want your password to contain upper case letters?")) {
     growingArray = growingArray.concat(upperCase);
   }
@@ -55,18 +57,22 @@ const chooseCategories = function () {
   if (confirm("Do you want your password to contain special characters?")) {
     growingArray = growingArray.concat(special);
   }
+  // If the array is empty, repeat the confirmation dialogues until the user chooses at least one category.
   if (growingArray.length == 0) {
     alert("You must choose at least one category.");
     chooseCategories();
+    // Once we have at least one category, assign the result to the global variable we declared earlier.
   } else {
-    console.log(growingArray);
     chosenCategories = growingArray;
   }
 };
 
-// Takes two arguments, 'n' and Builds the user's password by declaring an empty string, then randomly selecting items from the 'chosenCategories' array and concatenating them.
+// Build the password by creating an empty string, then randomly selecting characters from our new array containing all chosen categories and adding 
+// them to the string one at a time. Takes two arguments: 'n': the chosen password length; and 'arr': the array containing all chosen categories.
+// Once we have added enough characters, assign the result (i.e. our final password) to the global variable we initialised above.
 var buildPassword = function (n, arr) {
   var i = 0;
+  // Declare a variable to store the password while we're building it.
   var growingPassword = "";
   while (i < n) {
     var randomNumber = Math.floor(Math.random() * arr.length);
@@ -77,32 +83,20 @@ var buildPassword = function (n, arr) {
   finalPassword = growingPassword;
 };
 
+// Create a function which calls each of the above functions in sequence, then returns the value of our final password variable.
 var generatePassword = function () {
   chooseLength();
   chooseCategories();
   buildPassword(chosenLength, chosenCategories);
-  console.log(finalPassword); // Logs to console
   return finalPassword;
 };
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Add event listener to generate button.
 generateBtn.addEventListener("click", writePassword);
-
-/*
-  TODONE: series of prompts
-    TODONE: select length 
-      TODONE: between 8 and 128 characters
-    TODONE: character types to include
-      lowercase; uppercase; numeric; special characters
-    TODO: validates answers
-      TODONE: at least one selected
-  TODONE: generates password matching criteria
-    TODONE: either displayed in alert or written to page
-*/
